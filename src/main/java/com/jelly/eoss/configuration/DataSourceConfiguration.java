8e4bb1d023d3da0ee2d5fabe150546f671ae017a
jelly <195358385@qq.com>
2019-01-15 10:15:22 +0800

    add redis configuration

--- src/main/java/com/jelly/eoss/configuration/DataSourceConfiguration.java
+++ src/main/java/com/jelly/eoss/configuration/DataSourceConfiguration.java
@@ -1,17 +1,21 @@
 package com.jelly.eoss.configuration;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 import javax.sql.DataSource;
 
 /**
  * @Author ：jelly.liu
  * @Date ：Created At 9:40 AM 2019/1/10
- * @Description：${description}
+ * @Description：
+ * MySQL, Redis, RabbitMQ, ETC...
  */
 
 @Configuration
@@ -26,5 +30,17 @@ public class DataSourceConfiguration {
         dataSource.setUsername(env.getProperty("spring.datasource.username"));
         dataSource.setPassword(env.getProperty("spring.datasource.password"));
         return dataSource;
+    }
+
+    @Bean
+    public JedisPool jedisPool(){
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setTestOnBorrow(false);
+        JedisPool pool = new JedisPool(config,
+                env.getProperty("spring.redis.url"),
+                env.getProperty("spring.redis.port", Integer.class),
+                Integer.parseInt(StringUtils.substringBeforeLast(env.getProperty("spring.redis.timeout"), "MS")),
+                env.getProperty("spring.redis.passowrd"));
+        return pool;
     }
 }
